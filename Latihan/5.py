0e04e20be9432f6adc5bf2446c42d0a97d30e04a
NizarArhaNaufal <72216735+NizarArhaNaufal@users.noreply.github.com>
2020-10-17 12:34:43 +0700

    Add files via upload

---
+++ Latihan/5.py
@@ -0,0 +1,6 @@
+mahasiswa = 100
+mahasiswi = 150
+lakidiagram = mahasiswa // 10
+perempuandiagram = mahasiswi // 10
+print('laki:','*' *int(lakidiagram))
+print('perempuan:','*' *int(perempuandiagram))
